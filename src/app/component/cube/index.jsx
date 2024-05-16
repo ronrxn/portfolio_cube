e043b5c7a242a5383d5e8214581e3b2c326c90c2
romain <romain.equoy@epitech.eu>
2024-05-16 15:42:23 +0200

    fix des problemes de couleur

--- src/app/component/cube/index.jsx
+++ src/app/component/cube/index.jsx
@@ -20,9 +20,9 @@ export default function Index() {
     return (
         <div ref={container} className={styles.main}>
             <div className={styles.cube}>
-                <Canvas>
+                <Canvas toneMapped={false} antialias={true} linear>
                     <OrbitControls enableZoom={false} enablePan={false} />
-                    <ambientLight intensity={1} />
+                    <ambientLight intensity={2.5} />
                     <directionalLight position={[1, 1, 1]} />
                     <Cube progress={smoothProgress}>
                     </Cube>
@@ -37,16 +37,23 @@ function Cube({ progress }) {
 
     const mesh = useRef(null)
 
-    const texture_1 = useLoader(TextureLoader, "/assets/romain-e.png")
+
+    const texture_1 = useLoader(TextureLoader, "/assets/romain-e.jpg")
     const texture_2 = useLoader(TextureLoader, "/assets/IMG_6071.png")
+    const texture_3 = useLoader(TextureLoader, "/assets/IMG_6071.png")
+    const texture_4 = useLoader(TextureLoader, "/assets/IMG_6071.png")
+    const texture_5 = useLoader(TextureLoader, "/assets/IMG_6071.png")
+    const texture_6 = useLoader(TextureLoader, "/assets/IMG_6071.png")
 
     return (
         <motion.mesh ref={mesh} rotation-y={progress}>
             <boxGeometry args={[2.5, 2.5, 2.5]} />
-            <meshStandardMaterial map={texture_1} attach="material-4" />
+            <meshStandardMaterial map={texture_1} attach="material-4" toneMapped={false} />
             <meshStandardMaterial map={texture_2} attach="material-1" />
-            <meshStandardMaterial color={"white"} attach="material-2" />
-            <meshStandardMaterial color={"white"} attach="material-3" />
+            <meshStandardMaterial map={texture_3} attach="material-2" />
+            <meshStandardMaterial map={texture_4} attach="material-3" />
+            <meshStandardMaterial map={texture_5} attach="material-0" />
+            <meshStandardMaterial map={texture_6} attach="material-5" />
         </motion.mesh>
     )
 }
